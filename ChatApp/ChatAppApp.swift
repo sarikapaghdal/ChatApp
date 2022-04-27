f34d8eef99ee1ebe96e16182ef0ad153725ff34c
Sarika <sarika.paghdal1@gmail.com>
2022-04-27 11:30:25 +0200

    Initial Commit

---
+++ ChatApp/ChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppApp.swift
+//  ChatApp
+//
+//  Created by Sarika on 27.04.22.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
